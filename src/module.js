a9dca15d89c3571b2347e60facf369dab8b2ed23
unknown <nenegrakrina@gmail.com>
2021-03-06 22:21:32 +0400

    ya hochu spat

--- src/module.js
+++ src/module.js
@@ -1,1 +1,7 @@
 console.log('Module.js')
+
+async function start() {
+  await Promise.resolve()
+}
+
+start()
--- webpack.config.js
+++ webpack.config.js
@@ -1,59 +1,75 @@
-const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
-  entry: './index.js',
-  output: {
-    filename: 'bundle.[hash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core')
-    }
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HTMLWebpackPlugin({
-      template: 'index.html'
-    }),
-    new CopyPlugin([
-      {
-        from: path.resolve(__dirname, 'src/favicon.ico'),
-        to: path.resolve(__dirname, 'dist')
+    context: path.resolve(__dirname, 'src'),
+    mode: "development",
+    entry: './index.js',
+    output: {
+      filename: filename('js'),
+      path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+      extensions: ['.js'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        "@core": path.resolve(__dirname, 'src/core')
       }
-    ]),
-    new MiniCssExtractPlugin({
-      filename: 'bundle.[hash].css'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+    },
+    devtool: isDev ? 'source-map' : false,
+    devServer: {
+      port: 3000,
+      hot: isDev
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HTMLWebpackPlugin({
+        template: 'index.html',
+        minify: {
+          removeComments: isProd,
+          collapseWhitespace: isProd
+          }
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src/favicon.ico'),
+            to: path.resolve(__dirname, 'dist')
+          }
+        ]
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'bandle.[hash].css'
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+            use: [
+              MiniCssExtractPlugin.loader,
+              'css-loader',
+              'sass-loader'
+            ],
+          },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
         }
-      }
-    ]
-  }
+      ]
+    }
 }
